1fcb33473165c08453164f217da88de2d8de372b
Andreu <Andreu@192.168.15.134>
2016-01-26 18:15:01 +0100

    commit inicial

---
+++ GolejadorsUnits/src/main/java/net/andreu/GolejadorsUnits/App.java
@@ -0,0 +1,31 @@
+package net.andreu.GolejadorsUnits;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.SAXException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	private static String[] FITXER = {"/golejador.xml"};
+	
+    public static void main( String[] args ) throws ParserConfigurationException, SAXException, IOException
+    {
+        SAXParserFactory fabrica=SAXParserFactory.newInstance();
+        fabrica.setNamespaceAware(true);
+        SAXParser parser=fabrica.newSAXParser();
+        for(int i=0; i<FITXER.length; i++){
+        	InputStream File = App.class.getResourceAsStream(FITXER[i]);
+        	parser.parse(File,new Processar());
+        }
+        
+    }
+}
---
+++ GolejadorsUnits/src/main/java/net/andreu/GolejadorsUnits/Processar.java
@@ -0,0 +1,21 @@
+package net.andreu.GolejadorsUnits;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+public class Processar extends DefaultHandler {
+
+	public void startElement(String uri, String localName, String qName, Attributes attributes){
+		
+	}
+	public void characters(char[] ch, int start, int length) {
+		
+	}
+	public void endElement(String uri, String localName, String qName) throws SAXException {
+		
+	}
+	public void endDocument() {
+		
+	}
+}
